93a139bc81c5355e3211f9803724e1b3376d42fb
nzanni <nzanni98@gmail.com>
2025-06-09 18:55:58 -0300

    wip

--- Models/Alumno.cs
+++ Models/Alumno.cs
@@ -2,5 +2,15 @@
 {
     public class Alumno
     {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+
+        public string? Apellido { get; set; }
+
+        public int DNI { get; set; }
+
+        public string? Email { get; set; }
+
+        public DateTime FechaDeNacimiento { get; set; }
     }
 }
---
+++ Pages/CreateAlumno.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SistemaAcademico.Pages
+{
+    public class CreateAlumnoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
