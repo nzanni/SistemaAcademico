bacdd40dd6e43fe70de7c6647b43ad8c437bd9cd
nzanni <nzanni98@gmail.com>
2025-06-13 00:08:25 -0300

    Listo tabla Alumno

--- Data/DatosCompartidos.cs
+++ Data/DatosCompartidos.cs
@@ -4,13 +4,22 @@ namespace SistemaAcademico.Data
 {
     public static class DatosCompartidos
     {
-        public static List<Carrera> ListCarrera = new ();
+        public static List<Carrera> ListCarrera = new();
         private static int ultimoId = 0;
         public static int ObtenerNuevoId()
         {
             ultimoId++;
             return ultimoId;
         }
+        public static List<Alumno> ListAlumnos = new();
+        private static int ultimoIdAlumno = 0;
+        public static int ObtenerNuevoIdAlumno()
+        {
+            ultimoIdAlumno++;
+            return ultimoIdAlumno;
+        
+        }
+
     }
 
 }
--- Pages/CreateAlumno.cshtml.cs
+++ Pages/CreateAlumno.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SistemaAcademico.Data;
+using SistemaAcademico.Models;
 
 namespace SistemaAcademico.Pages
 {
     public class CreateAlumnoModel : PageModel
     {
+        [BindProperty]
+        public Alumno oAlumno { get; set; }
         public void OnGet()
         {
+        }
+        public static int ultimoIdAlumno = 0;
+        public IActionResult OnPost()
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            oAlumno.Id = DatosCompartidos.ObtenerNuevoIdAlumno();
+            DatosCompartidos.ListAlumnos.Add(oAlumno);
+            return RedirectToPage("TablaAlumnos");
         }
     }
 }
