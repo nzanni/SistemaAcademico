aad8f2c889f2e49e731c2461275d171f23611b16
nzanni <nzanni98@gmail.com>
2025-07-09 17:21:25 -0300

    Cambio de color de botones, arreglo de fecha y dni

--- Models/Alumno.cs
+++ Models/Alumno.cs
@@ -5,15 +5,26 @@ namespace SistemaAcademico.Models
     public class Alumno
     {
         public int Id { get; set; }
+
+
         [Required(ErrorMessage = "El nombre es obligatorio")]
         public string? Nombre { get; set; }
+
+
         [Required(ErrorMessage = "El apellido es obligatorio")]
         public string? Apellido { get; set; }
-        [StringLength(8, ErrorMessage ="Máximo 8 caracteres")]
+
+
+        [Range(10000000, 99999999)]
         public int DNI { get; set; }
+
+
         [Required(ErrorMessage = "El email es obligatorio")]
         public string? Email { get; set; }
 
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de nacimiento")]
         public DateTime FechaDeNacimiento { get; set; }
     }
 }
