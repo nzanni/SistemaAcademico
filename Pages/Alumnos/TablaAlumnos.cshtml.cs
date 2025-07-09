beeccf35982d3d1c3f99411ad4cb096e7edb577e
nzanni <nzanni98@gmail.com>
2025-07-09 20:20:46 -0300

    listo json Alumno

--- Pages/Alumnos/TablaAlumnos.cshtml.cs
+++ Pages/Alumnos/TablaAlumnos.cshtml.cs
@@ -2,15 +2,18 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SistemaAcademico.Data;
 using SistemaAcademico.Models;
+using SistemaAcademico.Servicios;
 
 namespace SistemaAcademico.Pages.Alumnos
 {
     public class TablaAlumnosModel : PageModel
+{
+    public List<Alumno> ListaMostrarAlumnos { get; set; }
+
+    public void OnGet()
     {
-        public List<Alumno> ListaMostrarAlumnos;
-        public void OnGet()
-        {
-            ListaMostrarAlumnos = DatosCompartidos.ListAlumnos;
-        }
+        ListaMostrarAlumnos = ServicioAlumno.ObtenerAlumnos();
     }
 }
+}
+
---
+++ Servicios/ServicioAlumno.cs
@@ -0,0 +1,27 @@
+﻿using SistemaAcademico.Models;
+using System.Text.Json;
+
+namespace SistemaAcademico.Servicios
+{
+    public class ServicioAlumno
+
+        {
+            private static string ruta = "Data/alumnos.json";
+
+            public static string LeerTextoDelArchivo()
+            {
+                if (File.Exists(ruta))
+                {
+                    return File.ReadAllText(ruta);
+                }
+                return "[]";
+            }
+            public static List<Alumno> ObtenerAlumnos()
+            {
+                string json = LeerTextoDelArchivo();
+
+                var lista = JsonSerializer.Deserialize<List<Alumno>>(json);
+                return lista ?? new List<Alumno>();
+        }
+    }
+}
